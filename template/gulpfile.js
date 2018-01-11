f213faf163ecdf588d52b6a957be6374059b3f39
Pieter Beulque <pieterbeulque@gmail.com>
2018-01-11 09:57:48 +0100

    Use Mr. Henry task registry from NPM

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,27 +1,7 @@
 const gulp = require('gulp');
+const MrHenry = require('gulp-registry-mrhenry');
 const config = require('./gulp/config');
 
-const css = require('./gulp/tasks/css');
-const fonts = require('./gulp/tasks/fonts');
-const icons = require('./gulp/tasks/icons');
-const images = require('./gulp/tasks/images');
-const { es6, babel, javascript } = require('./gulp/tasks/javascript');
+const tasks = new MrHenry(config);
 
-exports.css = css;
-exports.fonts = fonts;
-exports.images = images;
-
-gulp.task('javascript:es6', es6);
-gulp.task('javascript:babel', babel);
-exports.javascript = javascript;
-
-gulp.task('default', gulp.parallel(css, fonts, icons, images, javascript));
-
-const watch = () => {
-	gulp.watch(config.css.watch, css);
-	gulp.watch(config.images.src, images);
-	gulp.watch(config.fonts.src, fonts);
-	gulp.watch(config.js.watch, javascript);
-};
-
-gulp.task('watch', gulp.series('default', watch));
+gulp.registry(tasks);
