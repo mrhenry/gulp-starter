feeab912f7875f27b8189e46cf8b148c57a54daf
Pieter Beulque <pieterbeulque@gmail.com>
2018-02-19 15:12:25 +0100

    Remove legacy task files

--- template/gulp/tasks/fonts.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const config = require('../config').fonts;
-
-const fonts = () => gulp
-	.src(config.src, { since: gulp.lastRun(fonts) })
-	.pipe(gulp.dest(config.dest));
-
-module.exports = fonts;
